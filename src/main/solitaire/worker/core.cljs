a99dbc0cf19c0d69ecdb74cfe9efc6f904ac68fe
brian <biscuitalmighty@gmail.com>
2021-11-26 07:38:08 +0000

    Added service worker
    FossilOrigin-Name: 9d3edabf223eca14ced161ac095a8bf59c63d7054f2923c4a6a784450c2918e7

--- src/main/solitaire/worker/core.cljs
+++ src/main/solitaire/worker/core.cljs
@@ -1,4 +1,17 @@
-(ns solitaire.worker.core)
+(ns solitaire.worker.core
+  (:require ["workbox-routing" :as wb-routing]
+            ["workbox-strategies" :as wb-strategies]))
+
+(defn destination-handler [dests f]
+  (fn [opts]
+    (let [request (.-request opts)]
+      (when (contains? dests (.-destination request))
+        (f)))))
 
 (defn main []
-  (println "Hello, Worker!"))
+  (wb-routing/registerRoute
+    (destination-handler
+      #{"image" "script" "style"}
+      (fn []
+        (wb-strategies/StaleWhileRevalidate.
+          #js {:cacheName "stale"})))))
